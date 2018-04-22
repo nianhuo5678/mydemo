07fc5969e64e127c2eebcef87ef6ef36fb80a06c
nianhuo <nianhuo5678@163.com>
2018-04-22 13:42:57 +0800

    User class added

--- blog/models.py
+++ blog/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 from django.utils import timezone
 
+
 # Create your models here.
+class User(models.Model):
+    username = models.TextField(max_length=30, blank=True, null=True)
+    password = models.TextField(max_length=80, blank=True, null=True)
+

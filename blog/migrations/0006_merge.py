69ce033a2ccd0a3cc55de61d2fb2d6a9950e9033
nianhuo <nianhuo5678@163.com>
2018-05-18 00:03:44 +0800

    添加用户类、文章类和评论类

---
+++ blog/migrations/0006_merge.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('blog', '0004_auto_20180422_1446'),
+        ('blog', '0005_auto_20180422_1443'),
+    ]
+
+    operations = [
+    ]
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,25 @@
+# coding=utf-8
+
 from django.db import models
 from django.utils import timezone
 
 
-# Create your models here.
 class User(models.Model):
-    username = models.TextField(max_length=30, blank=True, null=True)
-    password = models.TextField(max_length=80, blank=True, null=True)
+    username = models.TextField(max_length=30, null=True)
+    password = models.TextField(max_length=80, null=True)
     is_admin = models.BooleanField(default=False)
-    info = models.TextField(max_length=1200, null=True, blank=True)
+    info = models.TextField(max_length=300, null=True, blank=True)
 
+
+class Article(models.Model):
+    title = models.TextField(max_length=50)
+    User = models.ForeignKey(User)
+    content = models.TextField(max_length=2000)
+    pub_date = models.DateField(default=timezone.now)
+
+
+class Comment(models.Model):
+    user = models.ForeignKey(User)
+    article = models.ForeignKey(Article)
+    content = models.TextField(max_length=500)
+    pub_date = models.DateField(default=timezone.now)
